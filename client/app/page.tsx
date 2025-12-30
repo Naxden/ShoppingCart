bdd42a5420b871c823adc615a904c7743bdec6e6
Naxden <kilarjan05@gmail.com>
2025-12-30 17:09:15 +0100

    Add better code formatting using Prettier integration as well as the IDE project files

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,16 +1,16 @@
-"use client";
+'use client'
 
-import { getProducts } from "@/app/lib/api";
-import ProductCard from "@/app/components/ProductCard";
-import { Product } from "@/app/types/Product";
-import { useEffect, useState } from "react";
+import { getProducts } from '@/app/lib/api'
+import ProductCard from '@/app/components/ProductCard'
+import { Product } from '@/app/types/Product'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([])
 
   useEffect(() => {
-    getProducts().then(setProducts);
-  }, []);
+    getProducts().then(setProducts)
+  }, [])
 
   return (
     <div className="p-8 grid grid-cols-1 md:grid-cols-3 gap-4">
@@ -20,5 +20,5 @@ export default function Home() {
         products.map((product) => <ProductCard key={product.id} {...product} />)
       )}
     </div>
-  );
+  )
 }
